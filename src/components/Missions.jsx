05be2d52d8e5ebfc8700d85897257f1ac8615e3e
jandui <janduineto18@gmail.com>
2023-05-04 16:10:20 -0300

    Requisito 9

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,22 @@
 import { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
 
 class Missions extends Component {
   render() {
+    const missionsRender = missions.map(({ name, year, country, destination }) => (
+      <Missions
+        key={ year }
+        name={ name }
+        year={ year }
+        country={ country }
+        destination={ destination }
+      />
+    ));
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missionsRender}
       </div>
     );
   }
