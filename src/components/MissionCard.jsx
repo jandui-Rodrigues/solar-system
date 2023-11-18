997f0c38b05797129f4f89d5def2e412380e3939
jandui <janduineto18@gmail.com>
2023-11-17 23:23:49 -0300

    refactor: init refactor in component missionCard

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -5,21 +5,29 @@ class MissionCard extends Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <article className="card-misson" data-testid="mission-card">
-        <h4 className="mission-name" data-testid="mission-name">{name}</h4>
+      <article
+        className="card-misson bg-zinc-950/60
+       border-zinc-50
+       border-solid
+       border rounded-xl min-h-[12rem] max-w-[31.25rem]"
+      >
+        <abbr className="no-underline" title={ name }>
+          <h4 className="mission-name truncate">{name}</h4>
+        </abbr>
+
         <hr />
-        <div>
+        <div className="flex text-[1.2rem] justify-between p-2">
           <div>
-            <p data-testid="mission-year">
+            <p className="">
               <ion-icon name="calendar-outline" />
               {year}
             </p>
-            <p data-testid="mission-country">
+            <p>
               <ion-icon name="location-outline" />
               {country}
             </p>
           </div>
-          <p data-testid="mission-destination">
+          <p>
             <ion-icon name="flag-outline" />
             {destination}
           </p>
